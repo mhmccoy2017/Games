9397da4dea8a96f8bea0d67d2c1acbea6220cf92
Mark McCoy <mhmccoy@email.sc.edu>
2022-05-23 16:54:30 -0400

    Created initial rotor run through
    Reworked the key value pairs for the alpha dictionary, created the first part of the encrypt method to encrypt any input string.

--- enigma.py
+++ enigma.py
@@ -4,10 +4,11 @@ import random
 
 #This pyhton file will be used to create a enigma encyrption of any input text using the Engima 1 schema
 
-aplhadir = {1 :'A', 2 : 'B', 3 : 'C', 4 :'D', 5:'E',
-6:'F',7:'G',8:'H',9:'I',10:'J',11:'K',
-12:'L',13:'M',14:'N',15:'O',16:'P',17:'Q',18:'R',19:'S',20:'T'
-,21:'U',22:'V',23:'W',24:'X',25:'Y',26:'Z'}
+#Global dict that is used as the core alphabet to letter conection for tranlsation in rotors
+aplhadir = {'A': 1, 'B' : 2, 'C' : 3, 'D':4, 'E':5,
+'F':6,'G':7,'H':8,'I':9,'J':10,'K':11,
+'L':12,'M':13,'N':14,'O':15,'P':16,'Q':17,'R':18,'S':19,'T':20
+,'U':21,'V':22,'W':23,'X':24,'Y':25,'Z':26}
 
 class Rotor:
 	'''
@@ -29,8 +30,10 @@ class Rotor:
 	def turn(self):
 		pass
 
-	def translate(self, letter, alphabet):
-		print(self.keys.get(letter))
+	def translate(self, letter):
+		global aplhadir
+		return self.keys.get(aplhadir.get(letter))
+		
 
 #A randomized plugboard that built for the engima to match letters
 class Plugboard:
@@ -67,12 +70,26 @@ class Enigma:
 	#Define a reflector value that will take in one character and then pass it through the rotors in reverse order
 
 	#Define an input method that will take any string and step through each letter of that string
+	def encrypt(self, msg):
+		global aplhadir
+		#Stripping the msg of spaces and sending it to upper case for matches
+		msg = msg.upper()
+		msg = msg.replace(" ", "")
+		#Init the out string
+		#For every character in the msg translate it per each rotor
+		temp = msg
+		for r in self.rotors:
+			out = ''
+			for char in temp:
+				out += r.translate(char)
+			temp = out
+		return temp
 
 r1 = Rotor('EKMFLGDQVZNTOWYHXUSPAIBRCJ', 'Q', 'Y')
 r2 = Rotor('AJDKSIRUXBLHWTMCQGZNPYFVOE', 'M', 'E')
 r3 = Rotor('BDFHJLCPRTXVZNYEIWGAKMUSQO', 'D', 'V')
 rs = [r1,r2,r3]
 pb = Plugboard()
-r1.translate('A', aplhadir)
 pb.create_board()
 E1 = Enigma(rs,pb)
+print(E1.encrypt('Hello my name is mark'))
