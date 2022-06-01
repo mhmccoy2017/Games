0bd1f5b93232ed86f6d9859785eeac9a2d38bd90
Mark McCoy <mhmccoy@email.sc.edu>
2022-06-01 14:28:17 -0400

    Finished Rotor turns and encryption

--- enigma.py
+++ enigma.py
@@ -10,10 +10,12 @@ aplhadir = {'A': 1, 'B' : 2, 'C' : 3, 'D':4, 'E':5,
 'L':12,'M':13,'N':14,'O':15,'P':16,'Q':17,'R':18,'S':19,'T':20
 ,'U':21,'V':22,'W':23,'X':24,'Y':25,'Z':26}
 
+#ADD LOGGING FUNCTIONS WITH ERROR CODES
+
 class Rotor:
 	'''
 	Creates a rotor with the key matching to number pairs
-	Has three varies key, notch, turnover
+	Has three variables key, notch, turnover
 	'''
 	def __init__(self, key, notch, turnover):
 		self.turnover = turnover
@@ -26,9 +28,13 @@ class Rotor:
 		return f'Keys: {self.keys}, turnover {self.turnover}, notch {self.notch}'
 
 	#Define a turn method when the ring hits it's notch it turns the key
-
 	def turn(self):
-		pass
+		temp = {}
+		for i, v in self.keys.items():
+			temp[i] = self.keys.get(i+1)
+		if temp[len(temp)] == None:
+			temp[len(temp)] = self.keys.get(1)
+		self.keys = temp
 
 	def translate(self, letter):
 		global aplhadir
@@ -45,6 +51,9 @@ class Plugboard:
 
 	def __str__(self):
 		return f'{self.plugboard}'
+
+	def get(self, char):
+		return self.plugboard.get(char)
 
 	#Creates a randomized pair of letters and saves them to plugboard dict, default input is 10 pairs 
 	def create_board(self, num = 10):
@@ -68,22 +77,37 @@ class Enigma:
 		return f'Plugboard: {self.plugboard}'
 
 	#Define a reflector value that will take in one character and then pass it through the rotors in reverse order
+	def reflector(self):
+		self.rotors = self.rotors[::-1]
 
 	#Define an input method that will take any string and step through each letter of that string
-	def encrypt(self, msg):
+	def teslate(self, msg):
 		global aplhadir
 		#Stripping the msg of spaces and sending it to upper case for matches
 		msg = msg.upper()
 		msg = msg.replace(" ", "")
-		#Init the out string
+		#Encrypt Message using plug board
+		msg_plug = ''
+		for char in msg: 
+			if self.plugboard.get(char) != None:
+				msg_plug += self.plugboard.get(char)
+			else:
+				msg_plug += char
 		#For every character in the msg translate it per each rotor
-		temp = msg
 		for r in self.rotors:
 			out = ''
-			for char in temp:
+			for char in msg_plug:
+				if r == self.rotors[0]:
+					r.turn()
 				out += r.translate(char)
-			temp = out
-		return temp
+			msg_plug = out
+		return msg_plug
+
+	def encrypt(self, msg):
+		first_pass = self.teslate(msg)
+		#reverses the rotors
+		self.reflector()
+		return self.teslate(first_pass)
 
 r1 = Rotor('EKMFLGDQVZNTOWYHXUSPAIBRCJ', 'Q', 'Y')
 r2 = Rotor('AJDKSIRUXBLHWTMCQGZNPYFVOE', 'M', 'E')
@@ -91,5 +115,6 @@ r3 = Rotor('BDFHJLCPRTXVZNYEIWGAKMUSQO', 'D', 'V')
 rs = [r1,r2,r3]
 pb = Plugboard()
 pb.create_board()
+print(pb)
 E1 = Enigma(rs,pb)
-print(E1.encrypt('Hello my name is mark'))
+print(E1.encrypt('Hello'))
